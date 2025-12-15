1fc23e1e859a5e6fa180d9fa4b974ba5c2b6f113
Andres <rekalcula@gmail.com>
2025-12-15 15:10:56 +0100

    Fix: remove unused userId prop from UploadForm

--- app/dashboard/upload/page.tsx
+++ app/dashboard/upload/page.tsx
@@ -24,7 +24,7 @@ export default async function UploadPage() {
             </p>
           </div>
 
-          <UploadForm userId={userId} />
+          <UploadForm />
         </div>
       </div>
     </>
