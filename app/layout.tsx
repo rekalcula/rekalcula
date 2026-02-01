c9fdf4bbd413ccd95cdc55ae086045ef88c656e4
Andres <rekalcula@gmail.com>
2026-02-01 21:17:08 +0100

    cambio texto rekalcula en email

--- app/layout.tsx
+++ app/layout.tsx
@@ -110,7 +110,6 @@ export default function RootLayout({
       <html lang="es">
         <head>
           <link rel="manifest" href="/manifest.json" />
-          <link rel="apple-touch-icon" href="/icons/icon-192x192.svg" />
           <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
           <meta name="apple-mobile-web-app-capable" content="yes" />
           <meta name="apple-mobile-web-app-status-bar-style" content="default" />
